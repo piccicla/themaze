cdd284e5bf96a9d4b095b0466d2b3143364f422c
claudio piccinini <picci2001@hotmail.it>
2022-01-29 20:10:58 +0000

    .

---
+++ src/player.rs
@@ -0,0 +1,48 @@
+////////////////
+/// player.rs
+/// Move and render the player
+/// authors: claudio piccinini picci2001@yahoo.it
+/// updated:29/01/22
+///
+use crate::prelude::*;
+//////////////
+
+pub struct Player{
+    pub position: Point
+}
+impl Player{
+    pub fn new(position: Point) -> Self {
+        Self {
+            position
+        }
+    }
+
+    pub fn render(&self, ctx:&mut BTerm){
+        ctx.set(
+            self.position.x,
+            self.position.y,
+            WHITE,
+            BLACK,
+            to_cp437('@'),
+        );
+    }
+
+    pub fn update(&mut self, ctx:&mut BTerm, map: &Map){
+        if let Some(key) = ctx.key{
+            let delta = match key {
+                VirtualKeyCode::Left => Point::new(-1,0),
+                VirtualKeyCode::Up => Point::new(0,-1),
+                VirtualKeyCode::Right => Point::new(1,0),
+                VirtualKeyCode::Down => Point::new(0,1),
+                _ => Point::zero()
+            };
+
+            let new_position = self.position + delta;
+            if map.can_enter_tile(new_position){
+                self.position = new_position;
+            }
+
+        }
+
+    }
+}
